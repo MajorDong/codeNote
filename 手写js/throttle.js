86bb13d8dd5464bb6e319f89b3049c24b58ce345
MajorDong <odsts@outlook.com>
2020-01-12 17:04:36 +0800

    throttle函数

---
+++ 手写js/throttle.js
@@ -0,0 +1,36 @@
+//应用比如说博客的自动保存
+//从上一次命令结束开始的一定时间范围t内，如果多次连续下达命令，则只执行当前时间段内第一次命令
+let gapTime = 3000 //3秒执行一次
+let lastTime = null
+let nowTime = null
+let fn = function(){
+  console.log('我执行了')
+}
+
+document.body.onscroll = function(){
+  nowTime = Date.now
+  if(!lastTime || nowTime - lastTime > gapTime){
+    fn()
+    lastTime = nowTime
+  } 
+}
+
+//封装
+let fn = function(){
+  console.log('我执行了')
+}
+throttleFn = throttle(fn, 3000)
+
+function throttle(fn, gapTime) {
+  let lastTime = null
+  let nowTime = null
+  return function(){
+    nowTime = Date.now()
+    if(!lastTime || nowTime - lastTime > gapTime){
+      fn()
+      lastTime = nowTime
+    } 
+  }
+} 
+
+document.body.onscroll = throttleFn
